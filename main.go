4fc014323b6e297cafecf7fb81aeeaf64b9335a0
瓦苏 <vasu.shori30@gmail.com>
2023-09-26 21:59:02 +0530

    added append and some comment regarding slice and append

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	//Slice can have any number of elements
+	//Slice element can be of only 1 type
 	cards := []string{"Ace of Diamonds", newCard()}
-	for i, card := range cards {
-		fmt.Println(i, card)
+	cards = append(cards, "Six of Hearts")
+	//append in the background create a new slice and do not modify the existing slice when we append new element
+	for _, card := range cards {
+		fmt.Println(card)
 	}
 }
 
