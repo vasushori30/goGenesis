9adf3386133ce14a8f58af191c89de0f6cf2b510
瓦苏 <vasu.shori30@gmail.com>
2023-09-26 22:13:19 +0530

    move for to another file

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 	//Slice can have any number of elements
 	//Slice element can only have 1 type
 	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Hearts")
 	//append in the background create a new slice and do not modify the existing slice when we append new element
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
+	cards = append(cards, "Six of Hearts")
+	cards.print()
 }
 
 func newCard() string {
--- state.go
+++ state.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 // Create a new type 'deck'
 // which is slice of strings
 type deck []string
+
+// d = cards as in main.go we have cards.print()
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
