fdde3d987406f06989eefb3338a1a067e9e70cd2
瓦苏 <vasu.shori30@gmail.com>
2023-09-26 22:32:47 +0530

    Added multiple for loop and append in declared slice

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 // To run multiple files of same package 'go run main.go state.go'
 func main() {
-	//Slice can have any number of elements
-	//Slice element can only have 1 type
-	cards := deck{"Ace of Diamonds", newCard()}
-	//append in the background create a new slice and do not modify the existing slice when we append new element
-	cards = append(cards, "Six of Hearts")
+	cards := newDeck()
 	cards.print()
 }
 
--- state.go
+++ state.go
@@ -6,6 +6,20 @@ import "fmt"
 // which is slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
+	cardValues := []string{"One", "Diamonds", "Hearts", "Club"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+	return cards
+}
+
 // d = cards as in main.go we have cards.print()
 func (d deck) print() {
 	for i, card := range d {
