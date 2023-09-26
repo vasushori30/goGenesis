6b104144adc4bf94e911f7f28997f3c2533c7621
瓦苏 <vasu.shori30@gmail.com>
2023-09-26 23:13:20 +0530

    define method using slice range and return multiple values

--- state.go
+++ state.go
@@ -26,3 +26,8 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+func deal(d deck, handSize int) (deck, deck) {
+	//slice range syntax d[startIndexIncluding:upToNotIncluding]
+	return d[:handSize], d[handSize:]
+}
