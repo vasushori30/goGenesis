24723751193adaf2d58fc0bdb5b517670a5ff31e
瓦苏 <vasu.shori30@gmail.com>
2023-09-26 16:22:20 +0530

    standard way to define method inside function

--- main.go
+++ main.go
@@ -2,13 +2,7 @@ package main
 
 import "fmt"
 
-var card string = "Ace of Spades"
-var average float64
-
 func main() {
-	num := 1
-	average = 0.5
+	card := "Ace of Spades"
 	fmt.Println(card)
-	fmt.Println(num)
-	fmt.Println(average)
 }
