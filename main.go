c9de23717b5695f309c3593f2467cb2bbe2f3ae7
瓦苏 <vasu.shori30@gmail.com>
2023-09-26 23:13:46 +0530

    call and handle multiple returned values

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 // To run multiple files of same package 'go run main.go state.go'
 func main() {
 	cards := newDeck()
-	cards.print()
+	currentHandCards, remainingCards := deal(cards, 4)
+	currentHandCards.print()
+	remainingCards.print()
 }
 
 func newCard() string {
