e72ae459fd1e4ea033bd2b56125d6e7d7614edfc
瓦苏 <vasu.shori30@gmail.com>
2023-09-27 15:29:46 +0530

    byte of slice

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
+import "fmt"
+
 // To run multiple files of same package 'go run main.go state.go'
 func main() {
-	cards := newDeck()
-	currentHandCards, remainingCards := deal(cards, 4)
-	currentHandCards.print()
-	remainingCards.print()
+	// cards := newDeck()
+	// currentHandCards, remainingCards := deal(cards, 4)
+	// currentHandCards.print()
+	// remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
