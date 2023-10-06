98abe0694eea2e861614b31fb0cd6c1fbee0ce03
瓦苏 <vasu.shori30@gmail.com>
2023-10-06 17:54:58 +0530

    check error handling

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // To run multiple files of same package 'go run main.go state.go'
 func main() {
-	cards := newDeckFromFile("test")
+	cards := newDeckFromFile("test1")
 	fmt.Println(cards)
 	// fmt.Println(cards.toString())
 	// currentHandCards, remainingCards := deal(cards, 4)
